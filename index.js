9b947c87b77fd9d0b0cd02108bfbe61682a94b4b
Myles Borins <myles@famo.us>
2015-05-01 23:53:03 -0700

    Smartly handle no #talkpay

--- index.js
+++ index.js
@@ -7,15 +7,31 @@ var T = new Twit(config);
 
 var stream = T.stream('user');
 
+function callbackHandler(err, data) {
+  if (err) {
+    return console.error(new Error(err));
+  }
+  return console.log(data);
+}
+
 stream.on('direct_message', function (eventMsg) {
   var msg = eventMsg.direct_message.text;
+  var screenName = eventMsg.direct_message.sender.screen_name;
+  
+  if (screenName === 'talkpayBot') {
+    return;
+  }
 
-  if (msg.search('#talkpay') !== -1) {
-    T.post('statuses/update', { status: msg }, function (err, data) {
-      if (err) {
-        return console.error(new Error(err));
-      }
-      return console.log(data);
-    });
+  else if (msg.search('#talkpay') !== -1) {
+    T.post('statuses/update', {
+      status: msg
+    }, callbackHandler);
+  }
+
+  else {
+    T.post('direct_messages/new', {
+      screen_name: screenName,
+      text: 'ruhroh, you need to include #talkpay in your DM for me to do my thang'
+    }, callbackHandler);
   }
 });
