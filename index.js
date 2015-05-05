6e3611ec1acd5232e7978cb1be7259c98b43f4c2
Myles Borins <myles@famo.us>
2015-05-04 23:18:33 -0700

    Super janky moderation

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 'use strict';
 
+var path = require('path');
+var basename = path.basename;
+
+var request = require('request');
 var Twit = require('twit');
+
 var config = require('./local.json');
+var moderators = require('./moderators.json');
+var includes = require('lodash.includes');
 
 var T = new Twit(config);
 
@@ -11,33 +18,67 @@ function callbackHandler(id) {
   T.post('direct_messages/destroy', {
     id: id
   }, function (err) {
+
     if (err) { console.error(err); }
   });
 }
+
 
 stream.on('direct_message', function (eventMsg) {
   var msg = eventMsg.direct_message.text;
   var screenName = eventMsg.direct_message.sender.screen_name;
   var msgID = eventMsg.direct_message.id_str;
-  
+  var badApple;
+
   if (screenName === 'talkpayBot') {
     return callbackHandler(msgID);
   }
 
+  else if (includes(moderators, screenName) && msg.search('#shitbird') !== -1) {
+
+    // share a tweet via DM with #shitbird
+    // msg = ['#shitbird', 'http://t.co/someshortthing']
+    // we need the full url though to extract the msgID we want to delete
+    try {
+
+      return request(msg.split(' ')[1], function (err, response) {
+        if (err) {
+          return callbackHandler(msgID);
+        }
+
+        badApple = basename(response.request.uri.pathname);
+
+        return T.post('statuses/destroy/:id', {
+          id: badApple
+        }, function () {
+
+          return callbackHandler(msgID);
+        });
+      });
+    }
+    catch (e) {
+      return callbackHandler(msgID);
+    }
+  }
+
   else if (msg.search('#talkpay') !== -1) {
+
     return T.post('statuses/update', {
       status: msg
     }, function () {
-      callbackHandler(msgID);
+
+      return callbackHandler(msgID);
     });
   }
 
   else {
     return T.post('direct_messages/new', {
+
       screen_name: screenName,
       text: 'ruhroh, you need to include #talkpay in your DM for me to do my thang'
     }, function () {
-      callbackHandler(msgID);
+
+      return callbackHandler(msgID);
     });
   }
 });
