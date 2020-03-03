d6dd6d9cf4222fbff3a3bb2bed480ed4dc147485
Myles Borins <myles.borins@gmail.com>
2020-03-03 05:16:06 -0500

    this is exciting

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ const auth = {
   access_token_secret: process.env.ACCESS_TOKEN_SECRET
 };
 
-const moderator = process.env.USER_ID;
+const moderatorID = process.env.MODERATOR_ID;
+const botID = process.env.BOT_ID;
 
 function validateWebhook(token, res) {
   console.log('Validating Webhook');
@@ -36,53 +37,64 @@ function validateWebhook(token, res) {
 async function handleDM(m) {
   if (m.type !== 'message_create') return;
   console.log('direct message received');
+
   const msg = m.message_create.message_data.text;
   const senderId = m.message_create.sender_id;
   const receiverId = m.message_create.target.recipient_id;
   const msgID = m.id;
   const urls = m.message_create.message_data.entities.urls;
-
-  await dm.destory(auth, msgID);
-  console.log('direct message destroyed')
-
-  let responseID;
+  
+  await dm.destroy(auth, msgID);
+  console.log('direct message destroyed');
 
   // if the message is from itself do nothing
-  if (senderId === receiverId) {
+  if (senderId === botID) {
     return;
   }
 
   // if the message is from a moderator and includes #shitbird delete the referenced tweet
   // share a tweet via DM with #shitbird
   // msg = ['#shitbird', 'http://t.co/someshortthing']
-  // we need the full url though to extract the msgID we want to delete
-  else if (senderId === moderator && msg.startsWith('#shitbird')) {
-    const id = msg.slice(10);
-    await tweet.destory(auth, id);
-    const responseID = await dm.send(auth, senderId, 'Moderated.');
-
+  // we need the full url though to extract the msgID we want to delete'
+  if (senderId == moderatorID && msg.startsWith('#cleanup')) {
+    const target = urls[0].expanded_url;
+    const split = target.split('/');
+    const id = split[split.length - 1];
+    try {
+      await tweet.destroy(auth, id);
+      await dm.send(auth, senderId, 'Moderated.');
+    }
+    catch (e) {
+      console.error(e);
+      await dm.send(auth, senderId, 'Something went wrong 😢.');
+    }
   }
 
   // No sharing links
   else if (urls.length) {
-    const responseID = await dm.send(auth, senderId, 'Sorry, I will not send out messages that include links.', senderId, msgID);
+    await dm.send(auth, senderId, 'Sorry, I will not send out messages that include links.', senderId, msgID);
   }
 
   // if the message includes #talkpay tweet it
-  else if (msg.search('#talkpay').includes('#talkpay')) {
-    const tweetID = await tweet.update(auth, msg);
-    const responseID = await dm.send(auth, senderId, 'I\'ve. shared your salary information and deleted all messages. Thanks for sharing 🎉');
+  else if (msg.includes('#talkpay')) {
+    try {
+      const tweetID = await tweet.update(auth, msg);
+      await dm.send(auth, senderId, 'I\'ve shared your salary information and deleted all messages. Thanks for sharing 🎉');
+    }
+    catch (e) {
+      console.error(e);
+      await dm.send(auth, senderId, 'Something went wrong, please try again.');
+    }
   }
 
   // if all else fails warn the messanger of what they need to do
   else {
-    const responseID = await dm.send(auth, senderId, 'You need to include #talkpay in your DM for me to do my thing');
+    await dm.send(auth, senderId, 'You need to include #talkpay in your DM for me to do my thing');
   }
-  
-  if (responseID) await dm.destory(auth, responseID);
 }
 
 async function handleWebhook(req) {
+  console.log('handling webhook')
   const dataBuffer = new BufferList();
   req.on('data', chunk => {
     dataBuffer.append(chunk);
@@ -90,13 +102,18 @@ async function handleWebhook(req) {
   req.on('end', async () => {
     const result = JSON.parse(dataBuffer.toString());
     const dms = result['direct_message_events'];
-    if (dms) {
-      await Promise.all(dms.map(handleDM));
+    if (dms && dms.length) {
+      try {
+        await Promise.all(dms.map(handleDM));
+      }
+      catch (e) {
+        console.error(e);
+      }
     }
   });
 }
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   console.log(`request received for: ${req.url}`);
 
   const route = url.parse(req.url, true);
@@ -117,6 +134,7 @@ const server = http.createServer((req, res) => {
     finally {
       res.end();
     }
+    return;
   }
   if (req.url !== '/') {
     res.writeHead(404);
